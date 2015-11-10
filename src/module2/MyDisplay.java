40cf9411efe9b3250039b7bcf97f7a6acdbf3cfb
Sudharaka Palamakumbura <sudharakap@mun.ca>
2015-11-09 21:48:20 -0330

    Initial Commit

---
+++ src/module2/MyDisplay.java
@@ -0,0 +1,20 @@
+package module2;
+
+import processing.core.PApplet;
+
+public class MyDisplay extends PApplet {
+	public void setup(){
+		size(400, 400);
+		background(200,100,200);
+	}
+	
+	public void draw(){
+		fill(255, 255, 0);
+		ellipse(200, 200, 390, 390);
+		fill(0,0,0);
+		ellipse(120, 120, 50, 50);
+		ellipse(280, 120, 50, 50);
+		noFill();
+		arc(200, 280, 100, 100, 0, PI, OPEN);
+	}
+}
